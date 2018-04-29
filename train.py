0dcab86467a702590dbfde30f908371bd632728f
Yang Liu <foamliu@yeah.net>
2018-04-29 20:58:18 +0800

    refactor and monitor val_acc instread of val_loss

--- train.py
+++ train.py
@@ -4,40 +4,65 @@ import keras
 
 from resnet_101 import resnet101_model
 from keras.preprocessing.image import ImageDataGenerator
+from keras.callbacks import CSVLogger, ModelCheckpoint, EarlyStopping
+from keras.callbacks import ReduceLROnPlateau
 
-IMG_WIDTH, IMG_HEIGHT = 224, 224
-TRAIN_DATA = 'fer2013/train'
-VALID_DATA = 'fer2013/valid'
-NUM_CLASSES = 7
-NB_TRAIN_SAMPLES = 28709
-NB_VALID_SAMPLES = 3589
-BATCH_SIZE = 32
-
+# parameters
+img_width, img_height = 227, 227
+num_channels = 3
+train_data = 'fer2013/train'
+valid_data = 'fer2013/valid'
+num_classes = 7
+num_train_samples = 28709
+num_valid_samples = 3589
+verbose = 1
+batch_size = 32
+num_epochs = 100
+patience = 50
 
 if __name__ == '__main__':
     # build a classifier model
-    model = resnet101_model(IMG_HEIGHT, IMG_WIDTH, 3, NUM_CLASSES)
+    model = resnet101_model(img_height, img_width, num_channels, num_classes)
 
     # prepare data augmentation configuration
-    train_data_gen = ImageDataGenerator(rescale=1. / 255, zoom_range=0.2, rotation_range=20,
-                                        width_shift_range=0.2, height_shift_range=0.2, horizontal_flip=True)
-    valid_data_gen = ImageDataGenerator(rescale=1. / 255, zoom_range=0.2, rotation_range=20,
-                                        width_shift_range=0.2, height_shift_range=0.2, horizontal_flip=True)
+    train_data_gen = ImageDataGenerator(featurewise_center=False,
+                                        featurewise_std_normalization=False,
+                                        rotation_range=10,
+                                        width_shift_range=0.1,
+                                        height_shift_range=0.1,
+                                        zoom_range=0.1,
+                                        horizontal_flip=True)
+    valid_data_gen = ImageDataGenerator(featurewise_center=False,
+                                        featurewise_std_normalization=False,
+                                        rotation_range=10,
+                                        width_shift_range=0.1,
+                                        height_shift_range=0.1,
+                                        zoom_range=0.1,
+                                        horizontal_flip=True)
 
-    train_generator = train_data_gen.flow_from_directory(TRAIN_DATA, (IMG_WIDTH, IMG_HEIGHT), batch_size=BATCH_SIZE,
-                                                         class_mode='categorical')
-    valid_generator = valid_data_gen.flow_from_directory(VALID_DATA, (IMG_WIDTH, IMG_HEIGHT), batch_size=BATCH_SIZE,
-                                                         class_mode='categorical')
+    # callbacks
+    tensor_board = keras.callbacks.TensorBoard(log_dir='./logs', histogram_freq=0, write_graph=True, write_images=True)
+    log_file_path = 'training.log'
+    csv_logger = CSVLogger(log_file_path, append=False)
+    early_stop = EarlyStopping('val_loss', patience=patience)
+    reduce_lr = ReduceLROnPlateau('val_loss', factor=0.1, patience=int(patience / 4), verbose=1)
+    trained_models_path = 'model'
+    model_names = trained_models_path + '.{epoch:02d}-{val_acc:.2f}.hdf5'
+    model_checkpoint = ModelCheckpoint(model_names, monitor='val_acc', verbose=1, save_best_only=True)
+    callbacks = [tensor_board, model_checkpoint, csv_logger, early_stop, reduce_lr]
 
-    tbCallBack = keras.callbacks.TensorBoard(log_dir='./logs', histogram_freq=0, write_graph=True, write_images=True)
+    # generators
+    train_generator = train_data_gen.flow_from_directory(train_data, (img_width, img_height), batch_size=batch_size,
+                                                         class_mode='categorical')
+    valid_generator = valid_data_gen.flow_from_directory(valid_data, (img_width, img_height), batch_size=batch_size,
+                                                         class_mode='categorical')
 
     # fine tune the model
-    history = model.fit_generator(
+    model.fit_generator(
         train_generator,
-        steps_per_epoch=NB_TRAIN_SAMPLES // BATCH_SIZE,
+        steps_per_epoch=num_train_samples / batch_size,
         validation_data=valid_generator,
-        validation_steps=NB_VALID_SAMPLES // BATCH_SIZE,
-        epochs=50,
-        callbacks=[tbCallBack])
-
-    model.save_weights("model.h5")
+        validation_steps=num_valid_samples / batch_size,
+        epochs=num_epochs,
+        callbacks=callbacks,
+        verbose=verbose)
